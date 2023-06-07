ab44ab066312e74a1df76f71682d75e1b854f279
Abed.Matini <abed.matini@ogilvy.co.za>
2023-06-07 21:20:36 +0200

    first

---
+++ src/app/new-listings/new-listings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-listings',
+  templateUrl: './new-listings.component.html',
+  styleUrls: ['./new-listings.component.css']
+})
+export class NewListingsComponent {
+
+}
